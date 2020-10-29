3bc6e85509ee45b0c457f87895220ff71206dfc3
luialbeto <araujo.script@gmail.com>
2020-10-29 01:57:40 -0300

    server and imp

---
+++ solar/server/routes/settings.js
@@ -0,0 +1,8 @@
+// change this
+
+export const MONGO_DB_NAME = 'example_db'
+export const MONGO_URL = 'mongodb://localhost:27017/'+MONGO_DB_NAME
+export const API_KEY = '__api_key__'
+
+// collection name
+export const USERS_COL = 'users'
